ceeaad6543baf2c441f0c7d8d56d3c6ab7d67dbe
caydt <87554270+caydt@users.noreply.github.com>
2021-08-06 15:25:03 +0900

    Update bticoinAutoTradWithMA.py

--- coin/bticoinAutoTradWithMA.py
+++ coin/bticoinAutoTradWithMA.py
@@ -56,7 +56,7 @@ while True:
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-BTC", krw*0.5)
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
